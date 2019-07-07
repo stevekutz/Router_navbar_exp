3c59103e01efc63576d73467eab4f426920000d6
stevekutz <stkutz@gmail.com>
2019-07-06 19:55:32 -0500

    test

--- src/components/People.js
+++ src/components/People.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+<<<<<<< HEAD
 
 export default function(props) {
   console.log("People props >> ", props);
@@ -15,14 +16,21 @@ return (
 
 /*
 function People(props) {
+=======
+const color = ["red", "yellow", "blue", "pink"];
+
+function People(props, match) {
+>>>>>>> 90c381fe0ec7f7d890c469414a1e932b92a20bf6
   console.log("People props >> ", props);
+  console.log("People idVal >> ", props.idVal);
 
    const paramVal = props.props.match.params.id;
    // console.log("paramVal is ", paramVal);
 
   return (
     <h4 key={props.index}>
-      Person {props.peep} {props.idVal}
+      at idVal {props.idVal}, person {props.peep} {props.test}
+      {color[`${props.idVal}`]}
     </h4>
   );
 }
