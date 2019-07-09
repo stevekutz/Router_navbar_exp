776bea68c51b69b027033e90db82145b03dd04ba
stevekutz <stkutz@gmail.com>
2019-07-08 19:12:16 -0500

    moved content into App

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom"; // wrap App !!!
+
+import PeoplePage from "./components/PeoplePage";
+import People from "./components/People";
+
+import "./styles.css";
+
+const Home = () => <h4> Home</h4>;
+// const People = props => <h4>People {props.index}</h4>;
+const About = () => <h4> About</h4>;
+
+export default function App() {
+  return (
+    <div className="App">
+      <nav className="navbar-container">
+        <h3> Some basic Routes </h3>
+        <div className="navbar">
+          <div className="divLink">
+            <Link to="/">Home </Link>
+          </div>
+          <div className="divLink">
+            <NavLink to="/about">About</NavLink>
+          </div>
+          <div className="divLink">
+            <NavLink to="/peoplepage">People Page</NavLink>
+          </div>
+        </div>
+      </nav>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/people/:id" exact component={People} />
+      <Route path="/about" component={About} />
+      <Route path="/peoplepage" component={PeoplePage} />
+    </div>
+  );
+}
--- src/components/People.js
+++ src/components/People.js
@@ -1,32 +1,14 @@
 import React from "react";
 
-<<<<<<< HEAD
-
-export default function(props) {
-  console.log("People props >> ", props);
-
-return (
-  <h4 key = {props.index}>
-   Person {props.peep} {props.idVal} {props.index}
-  </h4>
-)
-
-}
-
-
-/*
-function People(props) {
-=======
 const color = ["red", "yellow", "blue", "pink"];
 
 function People(props, match) {
->>>>>>> 90c381fe0ec7f7d890c469414a1e932b92a20bf6
   console.log("People props >> ", props);
   console.log("People idVal >> ", props.idVal);
 
-   const paramVal = props.props.match.params.id;
-   // console.log("paramVal is ", paramVal);
-
+  // const paramVal = props.match.params.id;
+  // console.log("paramVal is ", paramVal);
+  // extra
   return (
     <h4 key={props.index}>
       at idVal {props.idVal}, person {props.peep} {props.test}
@@ -36,4 +18,3 @@ function People(props, match) {
 }
 
 export default People;
-*/
